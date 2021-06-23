ac676cc5b96c599d585cf3cf79e787ce7379c1c0
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-06-23 20:30:00 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,36 @@
+img="";
+status="";
+function preload()
+{
+    img=loadImage("laptop.jpg");
+}
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+  
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model is loaded");
+    status= true;
+    objectDetector.detect(img,gotResult);
+    
+}
+function gotResult(error,results)
+{
+if(error)
+{
+console.error(error);
+}
+else{
+    console.log(results);
+}
+}
+function draw()
+{
+    image(img,0,0,640,420);
+    
+}
